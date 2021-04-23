7428828cd48523d76c17a88a6f539ce92f3d3c75
Mehrdad ekbatani <59129726+Mkbatni@users.noreply.github.com>
2021-04-23 12:26:11 -0700

    last edit

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-console.log("Hello");
-console.log("Ehwats up ");
+console.log("This is App json..")
+console.log("This is second line..")
+console.log("This is third line..")
+console.log("This is forth line..")
